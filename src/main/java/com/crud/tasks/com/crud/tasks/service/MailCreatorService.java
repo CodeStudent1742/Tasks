3b7b9ba811e6f27fbc3da2aad8377ce5f8f01662
Albert P <albert1742@gmail.com>
2023-01-31 12:41:16 +0100

    32.2 IWP

--- src/main/java/com/crud/tasks/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/com/crud/tasks/service/MailCreatorService.java
@@ -1,6 +1,7 @@
 package com.crud.tasks.com.crud.tasks.service;
 
 import com.crud.tasks.com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.com.crud.tasks.config.CompanyConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -13,15 +14,13 @@ public class MailCreatorService {
 
     @Autowired
     private AdminConfig adminConfig;
+
+    @Autowired
+    private CompanyConfig companyConfig;
     @Autowired
     @Qualifier("templateEngine")
     private TemplateEngine templateEngine;
 
-//    public String buildTrelloCardEmail(String message) {
-//        Context context = new Context();
-//        context.setVariable("message", message);
-//        return templateEngine.process("mail/created-trello-card-mail", context);
-//    }
 public String buildTrelloCardEmail(String message) {
     Context context = new Context();
     context.setVariable("message", message);
